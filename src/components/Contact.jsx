7507831d1d38e396481c29e9568c0fe75eeef06c
HRISHIKESH BHAGAT <hrishibhagat08@gmail.com>
2022-09-15 11:00:56 +0530

    responsive updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
 const Contact = () => {
-    return (
-        <h1>iam Contact</h1>
-    )
+  return (
+    <div className="about-child-sec">
+      <h1>About Us</h1>
+      <div>
+        <div className="about-child-sec-one">
+          <span>
+            <h2>Address :</h2>
+
+            <p>
+              {" "}
+              BCCI-IPL 4th Floor, Cricket Centre. Wankhede Stadium{" "}
+              <span>
+                {" "}
+                <br />{" "}
+              </span>{" "}
+              'D' Road, Churchgate Mumbai - 400020 India
+            </p>
+          </span>
+        </div>
+        <div className="about-child-sec-two">
+          <div>
+            <h1>contact us:</h1>
+            <ul>
+              <li> +91 22 22800300</li>
+              <li> +91 22 22800300</li>
+              <li> +91 22 22800300</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
